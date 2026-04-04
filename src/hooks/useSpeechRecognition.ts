3a4f1eccfe6fd358364e1a2fe7f2d81b62f42191
MoseLu <120814142@qq.com>
2026-04-05 01:05:25 +0800

    Restore speech and wrong-word feature facades

---
+++ src/hooks/useSpeechRecognition.ts
@@ -0,0 +1,5 @@
+export { useSpeechRecognition } from '../features/speech/hooks/useSpeechRecognition'
+export type {
+  SpeechRecognitionOptions,
+  UseSpeechRecognitionReturn,
+} from '../features/speech/hooks/useSpeechRecognition'
