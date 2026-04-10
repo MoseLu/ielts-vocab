c1435c977b1ab818355b05bfe6915cf135c6f4ff
MoseLu <120814142@qq.com>
2026-04-10 21:33:43 +0800

    update services learner profile application.py

---
+++ backend/services/learner_profile_application.py
@@ -0,0 +1,10 @@
+from platform_sdk.learner_profile_application_support import (
+    build_learner_profile_payload,
+    build_learner_profile_response,
+)
+
+
+__all__ = [
+    'build_learner_profile_payload',
+    'build_learner_profile_response',
+]
