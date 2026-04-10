cefa17e1056a07d383f7ad3e4cedaaa096a4fb79
MoseLu <120814142@qq.com>
2026-04-10 21:33:51 +0800

    update services learner profile repository.py

--- backend/services/learner_profile_repository.py
+++ backend/services/learner_profile_repository.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from datetime import datetime
 
-from models import (
+from service_models.learner_profile_models import (
     CustomBook,
     UserAddedBook,
     UserBookProgress,
